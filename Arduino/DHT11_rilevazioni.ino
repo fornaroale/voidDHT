fbf4d81b5e972b857b578e571cc438fe3154a2b5
Alessandro <aleforni97@gmail.com>
2017-02-18 13:02:30 +0100

    Update DHT11_rilevazioni.ino

--- Arduino/DHT11_rilevazioni.ino
+++ Arduino/DHT11_rilevazioni.ino
@@ -1,20 +1,20 @@
 #include <dht.h>		// includo libreria DHT
 #define DHT11_PIN 8		// PIN DHT: 8
-dht DHT;				// definisco componente usato (DHT11)
+dht DHT;			// definisco componente usato (DHT11)
 
 
 void setup(){
-	Serial.begin(9600);	// inizializzazione seriale
+	Serial.begin(9600);			// inizializzazione seriale
 	Serial.println("SENSORE DHT ");		// messaggio
 	Serial.print("VERSIONE LIBRERIA: ");	// messaggio
-	Serial.println(DHT_LIB_VERSION);		// messaggio
-	Serial.println();						// messaggio
+	Serial.println(DHT_LIB_VERSION);	// messaggio
+	Serial.println();			// messaggio
 	Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)");	// messaggio
 }
 
 
 void loop(){
-	Serial.print("DHT11, \t");			// messaggio
+	Serial.print("DHT11, \t");		// messaggio
 	int chk = DHT.read11(DHT11_PIN);	// leggo temp. in Celsius e umidità
   
 	switch (chk){	//switch di controllo (in caso di errori)
@@ -33,7 +33,7 @@ void loop(){
 	}
   
 	Serial.print(DHT.humidity,1);		// stampo umidità
-	Serial.print(",\t");				// vado a capo
+	Serial.print(",\t");			// vado a capo
 	Serial.println(DHT.temperature,1);	// stampo temperatura
 
 	delay(1000);	// aspetto 1 sec. per la prossima rilevazione
