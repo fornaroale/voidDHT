3e16379e2aae6ab3e4633709362ae3018a0d1a29
stemarzo <marzoste3@gmail.com>
2017-02-27 18:22:43 +0100

    Aggiunta commenti

--- Java/socketRilevazioni/src/RilevazioniServer.java
+++ Java/socketRilevazioni/src/RilevazioniServer.java
@@ -6,12 +6,13 @@ import java.util.logging.Logger;
  
 public class RilevazioniServer extends Thread
 {
+// -------------------------- DICHIARAZIONI SERVER SOCKET
    private ServerSocket serverSocket;
      
    public RilevazioniServer(int port) throws IOException
    {
-      serverSocket = new ServerSocket(port);
-      serverSocket.setSoTimeout(120000);//dopo 120 secondi la porta del Server va in time out
+      serverSocket = new ServerSocket(port);//Apro la porta del server
+      serverSocket.setSoTimeout(120000);//tempo dopo cui la porta del server va in time out(120sec)
    }
  
    public void run()
@@ -21,7 +22,7 @@ public class RilevazioniServer extends Thread
             try
             {
                 System.out.println("Server: Aspettando client sulla porta " + serverSocket.getLocalPort() + "...");
-                Socket server = serverSocket.accept();
+                Socket server = serverSocket.accept();//Accetto la richiesta di connessione fatta dal client al server 
                 System.out.println("Server: Connesso a " + server.getRemoteSocketAddress());
 
                 //DataInputStream in = new DataInputStream(server.getInputStream());
@@ -29,14 +30,13 @@ public class RilevazioniServer extends Thread
                 //DataOutputStream out = new DataOutputStream(server.getOutputStream());
                 //out.writeUTF("Certo! Ecco la lista.");
 
-                InputStream inFromClient = server.getInputStream();
-                DataInputStream in = new DataInputStream(inFromClient);
-
-                ObjectInputStream objectIn = new ObjectInputStream(inFromClient);
-                ArrayList<Rilevazione> rilevazioni = (ArrayList<Rilevazione>) objectIn.readObject();
-                for(int i=0; i<rilevazioni.size(); i++)
+                InputStream inFromClient = server.getInputStream();//Apro lo stream in input con il client
+                DataInputStream in = new DataInputStream(inFromClient);//Apro lo stream per ricevere dati dal client
+                ObjectInputStream objectIn = new ObjectInputStream(inFromClient);//Apro lo stream per ricevere oggetti dal client
+                ArrayList<Rilevazione> rilevazioni = (ArrayList<Rilevazione>) objectIn.readObject();//ricevo l'arraylist delle rilevazioni
+                for(int i=0; i<rilevazioni.size(); i++)//la scorro con un ciclo for
                 {
-                    scriviRilevazioneSuFile(rilevazioni.get(i).getTemp() + "," + rilevazioni.get(i).getHum());
+                    scriviRilevazioneSuFile(rilevazioni.get(i).getTemp() + "," + rilevazioni.get(i).getHum());//infine la stampo su file
                 }
 
                 server.close();
@@ -53,22 +53,27 @@ public class RilevazioniServer extends Thread
         }
     }
     
+  /**
+		 Questo metodo prende la rilevazione ricevuta e la scrive su file
+		 @param La rilevazione che deve essere scritta sul file
+	*/
     public void scriviRilevazioneSuFile(String rilevazione){
-        try(FileWriter fw = new FileWriter("rilevazioni.txt", true);
+        try(//Creazione dello stream per la scrittura su file
+            FileWriter fw = new FileWriter("rilevazioni.txt", true);
             BufferedWriter bw = new BufferedWriter(fw);
             PrintWriter out = new PrintWriter(bw))
         {
-            out.println(rilevazione);
+            out.println(rilevazione);//scrivo su file la rilevazione
         } catch (IOException e) {}
     }
    
     public static void main(String [] args)
     {
-        int port = Integer.parseInt("6066");
+        int port = Integer.parseInt("6066");//Porta che devo utilizzare 
         try
         {
-            Thread t = new RilevazioniServer(port);
-            t.start();
+            Thread t = new RilevazioniServer(port);//creo un nuovo thread
+            t.start();//faccio partire il thread
         }catch(IOException e) {
             e.printStackTrace();
         }
